cef7c5298800d26bdf5992f572cc7313f14e73d7
gauenk <kent.gauen@gmail.com>
2021-11-18 18:15:35 -0500

    pylib init fixed

--- pylib/__init__.py
+++ pylib/__init__.py
@@ -5,7 +5,3 @@ from .flow.interface import runPyTvL1Flow,runPyFlow,runPyFlowFB
 from .video_io.interface import readVideoForVnlb,readVideoForFlow
 from .flow.flow_utils import flow2img,flow2burst
 from .utils import compute_psnrs,expand_flows,rgb2bw
-<<<<<<< HEAD
-=======
-from .flow.flow_utils import flow2img,flow2burst
->>>>>>> 2251d472bd4a24e9dccfad06e47ac69409431b8c
